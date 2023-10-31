274d4b839f55eb97a3349b27eaff4681e568e955
LemonMenosQInexperto <sebastian.menafuentealba8@gmail.com>
2023-10-31 19:41:41 -0300

    hice un ventana
    eso.

---
+++ ProyectoTanques/src/main/java/com/sebabenja/proyectotanques/ObjTanque.java
@@ -0,0 +1,14 @@
+package com.sebabenja.proyectotanques;
+
+public class ObjTanque {
+    int x; //posicion x
+    int y; //posicion y
+    float angulo; //angulo
+    
+         
+    
+    
+    public ObjTanque()
+    {}
+    
+}
---
+++ ProyectoTanques/src/main/java/com/sebabenja/proyectotanques/ProyectoTanques.java
@@ -0,0 +1,18 @@
+package com.sebabenja.proyectotanques;
+
+import javax.swing.JFrame;
+
+public class ProyectoTanques {
+
+    public static void main(String[] args) {
+        
+        
+        final int P_ancho = 640;
+        final int P_alto = 480;
+        
+        JFrame pantalla = new JFrame();
+        pantalla.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pantalla.setVisible(true);
+        pantalla.setTitle("Progra2TanquePo");
+    }
+}
